2aa137e764105f678f1526c9bf14afb1e700d585
indritalls <95203855+indritalls@users.noreply.github.com>
2021-12-08 13:25:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,53 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    d = {'VENEZUELA':'CARACAS', 'CANADA':'OTTAWA'}
-    dc = random.choice(list(d.keys()))
+    t = {'Kalau kamu bisa jadi tidak terlihat, apa hal pertama yang akan kamu lakukan?':1, 
+        'Apa rahasia yang kamu sembunyikan dari orangtuamu?':2,
+        'Siapa orang yang diam-diam kamu sukai?' :3,
+        'Siapa orang terakhir yang kamu kepoin di media sosial?':4,
+        'Kalau ada jin yang memberikanmu tiga permohonan, apa yang kamu inginkan?':5,
+        'Jika kamu kembali ke masa lalu, apa yang akan kamu lakukan?':6,
+        'Apa tontonan favoritmu saat masih kecil?': 7,
+        'Siapa orang yang paling sering kamu chat?':8,
+        'Apa kebohongan terbesar yang pernah kamu katakan kepada orangtuamu?':9,
+        'Apa mimpi paling aneh yang pernah kamu alami?':10,
+        'Ceritakan detail ciuman pertamamu…':11,
+        'Kapan terakhir kali kamu ngompol atau eek di celana?':12,
+        'Menurutmu, hewan apa yang terlihat paling mirip denganmu?':13,
+        'Di antara temanmu, siapa orang yang paling kamu suka dalam konteks romantis?':15,
+        'Di antara temanmu, siapa orang yang menurutmu paling baik dan paling buruk sifatnya?':16,
+        'Siapa mantan terindahmu?':16,
+        'Siapa orang yang ingin kamu jadikan istri/suami?':17,
+        'Apakah kamu pernah melakukan ghosting?':18,
+        'Apa aib yang kamu sembunyikan dari teman-temanmu?':19,
+        'Berapa jumlah mantanmu? sebutkan!':20,
+        }
+    tth = random.choice(list(t.keys()))
+
+
+    d = {'Lakukan rap gaya bebas selama 3 menit!':1, 
+        'Biarkan orang lain membuat status menggunakan akun sosial mediamu!':2,
+        'Berikan ponselmu kepada salah satu di antara kita dan biarkan orang tersebut mengirim satu pesan kepada siapapun yang dia mau!' :3,
+        'Cium salah satu kaus kaki di antara temanmu!':4,
+        'Makan satu gigitan kulit pisang!':5,
+        'Peragakan salah satu orang di antara kita sampai ada yang bisa menebak siapa orang yang diperagakan!':6,
+        'Nyanyikanlah salah lagu lagu dari Rossa!': 7,
+        'Tirukan seorang selebriti sampai ada yang bisa menebak!':8,
+        'Bertingkahlah seperti Hotman Paris selama 2 menit!':9,
+        'Biarkan satu orang menggambar tato di wajahmu!':10,
+        'Tutuplah mata lalu raba muka salah satu di antara kita sampai kamu bisa menebak siapa orang itu!':11,
+        'Ungkapkan persaanmu kepada gebetanmu!':12,
+        'Push up 20 kali!':13,
+        'Kayang selama satu menit!':15,
+        'Plank selama satu menit!.':16,
+        'Coba teriak “aku sayang kamu” sekarang juga!':16,
+        'Baca dengan lantang pesan yang terakhir kali kamu kirim ke gebetanmu!':17,
+        'Telepon seorang teman dan katakan selamat ulang tahun sambil menyanyikan lagu!':18,
+        'Tunjukkan gerakan dance terbaikmu!':19,
+        'Parodikan adegan di film India kesukaanmu!':20,
+        }
+    dare = random.choice(list(d.keys()))
+
     msg_from_user = event.message.text
     if msg_from_user == 'Tes gambar':
         line_bot_api.reply_message(
@@ -42,13 +87,24 @@ def handle_message(event):
             original_content_url='https://1.bp.blogspot.com/-eaDZ7sDP9uY/Xhwqlve5SUI/AAAAAAABXBo/EcI2C2vim7w2WV6EYy3ap0QLirX7RPohgCNcBGAsYHQ/s400/pose_syanikamaeru_man.png',
             preview_image_url='https://1.bp.blogspot.com/-eaDZ7sDP9uY/Xhwqlve5SUI/AAAAAAABXBo/EcI2C2vim7w2WV6EYy3ap0QLirX7RPohgCNcBGAsYHQ/s400/pose_syanikamaeru_man.png'))
 
-    if msg_from_user == 'games':
-        message = TextSendMessage("Mau pilih truth atau dare?" + "\nPilih 1 untuk truth" + "\nPilih 2 untuk dare")
+    if msg_from_user == 'mulai':
+        message = TextSendMessage("Disini saya akan menampilkan peraturan selama games berlangsung" +
+        "\nPertama kamu akan disuruh memilih truth atau dare, Setelah memilih kamu diharuskan melakukan perintah yang sudah diberikan." + 
+        "\nJika tidak berhasil melakukan perintah dengan benar, maka akan muncul hukuman yang harus dijalani oleh peserta " + 
+        "\n"+ "\n"
+        "Kamu Mau pilih truth atau dare?" + 
+        "\nketik 'truth' untuk memulai games truth" + 
+        "\nketik 'dare' untuk memulai games dare")
         line_bot_api.reply_message(event.reply_token, message)
         
-    if msg_from_user == 'satu':
-        message = TextSendMessage(dc)
+    if msg_from_user == 'truth':
+        message = TextSendMessage(tth)
         line_bot_api.reply_message(event.reply_token, message)
+
+    if msg_from_user == 'dare':
+        message = TextSendMessage(dare)
+        line_bot_api.reply_message(event.reply_token, message)
+
 
 import os
 if __name__ == "__main__":
