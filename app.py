85da3121b68c9bf022968b9e62c12e4e1ed7954f
indritalls <95203855+indritalls@users.noreply.github.com>
2021-12-16 18:44:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ def handle_message(event):
         )   
         line_bot_api.reply_message(event.reply_token, message)
 
-    if msg_from_user == 'pilihan':
+    if msg_from_user == 'selesai':
         message = TemplateSendMessage(
             alt_text='Confirm template',
             template=ConfirmTemplate(
