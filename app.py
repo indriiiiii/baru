d3a1471dc26b2317183c54cbd41cf84891f1a676
indriiiiii <97089362+indriiiiii@users.noreply.github.com>
2022-01-04 15:52:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def handle_message(event):
         )   
         line_bot_api.reply_message(event.reply_token, message)
         
-    elif msg_from_user == 't':
+    if msg_from_user == 't':
         message = TemplateSendMessage(
             alt_text='Confirm template',
             template=ConfirmTemplate(
@@ -128,7 +128,7 @@ def handle_message(event):
         )   
         line_bot_api.reply_message(event.reply_token, message)
 
-    elif msg_from_user == 'd':
+    if msg_from_user == 'd':
         message = TemplateSendMessage(
             alt_text='Confirm template',
             template=ConfirmTemplate(
@@ -137,7 +137,7 @@ def handle_message(event):
         )   
         line_bot_api.reply_message(event.reply_token, message)
     
-    elif msg_from_user == 'bisa':
+    if msg_from_user == 'bisa':
         message = TemplateSendMessage(
             alt_text='Confirm template',
             template=ConfirmTemplate(
@@ -156,21 +156,21 @@ def handle_message(event):
         )   
         line_bot_api.reply_message(event.reply_token, message)
 
-    elif msg_from_user == 'gabisa':
+    if msg_from_user == 'gabisa':
         image_message = ImageSendMessage(
             original_content_url=gambar,
             preview_image_url='https://i.pinimg.com/564x/40/1e/cf/401ecf89c1d2cbac56d26cc95c3f9fb2.jpg'
             )
         line_bot_api.reply_message(event.reply_token, image_message) 
         
-    elif msg_from_user == 'aturan':
+    if msg_from_user == 'aturan':
         image_message = ImageSendMessage(
             original_content_url='https://i.pinimg.com/564x/53/25/ea/5325eab320dc87fcc72754708983abd4.jpg',
             preview_image_url='https://i.pinimg.com/564x/53/25/ea/5325eab320dc87fcc72754708983abd4.jpg'
         )
         line_bot_api.reply_message(event.reply_token, image_message)
     
-    elif msg_from_user == 'berhenti':
+    if msg_from_user == 'berhenti':
         sticker_message = StickerSendMessage(
             package_id='11537',
             sticker_id=stiker)
